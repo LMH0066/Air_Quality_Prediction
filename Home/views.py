b5724d8b148995b4f281b969d9a4757392b90d1a
liminghong <lmh0066@outlook.com>
2020-09-02 09:20:30 +0800

    加入csrf

--- Home/views.py
+++ Home/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 import json, os
 from Air_Quality_Prediction.settings import MEDIA_ROOT, MEDIA_URL
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
 # 返回全国全部站点的检测数据
+@csrf_exempt
 def get_china_aqi(request):
     path = os.path.join(MEDIA_ROOT, "data.json")
     # 读取json并进行分析
@@ -18,12 +20,13 @@ def get_china_aqi(request):
         for data in datas:
             for d in data:
                 if d['aqi']:
-                    results.append({'longitude': d['longitude'], 'latitude': d['latitude'], 'aqi': d['aqi']})
+                    results.append(d)
         return HttpResponse(json.dumps({'status': 0, 'data': results}))
     return HttpResponse(json.dumps({'status': 1}))
 
 
 # 预测
+@csrf_exempt
 def predict(request):
     longitude = request.POST.get('longitude')
     latitude = request.POST.get('latitude')
